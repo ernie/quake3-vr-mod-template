3cf48be4b42fda81d04d93ceb3038c223664cc45
Ernie Miller <ernie@ernie.io>
2026-07-19 21:18:36 -0400

    Step 2: wire the build
    The drop joins the file lists per the guide's build table: vr_bg in every
    module; vr_game in game; vr_cgame in cgame; vr_platform in cgame and both
    UIs; vr_ui per-UI; vr_uishared in the missionpack cgame and UI (the one
    dual-link case); the five baseq3 settings screens in the baseq3 UI.
    
    All six QVMs link. The modules are fully dormant: nothing calls them yet,
    so dead-code elimination strips them (including the API sentinel) until
    the call-outs land in Steps 4-6.

--- build/srcs.mk
+++ build/srcs.mk
@@ -8,7 +8,7 @@ QA_SRC = g_main $(QADIR)/g_syscalls.asm \
   bg_misc bg_pmove bg_slidemove bg_lib \
   g_active g_arenas g_bot g_client g_cmds g_combat g_items g_mem g_misc \
   g_missile g_mover g_session g_spawn g_svcmds g_target g_team g_trigger \
-  g_utils g_weapon \
+  g_utils g_weapon vr_bg vr_game \
   q_math q_shared
 
 ifeq ($(CONFIG),missionpack)
@@ -18,12 +18,13 @@ CG_SRC = cg_main $(CGDIR)/cg_syscalls.asm \
   cg_consolecmds cg_newdraw cg_draw cg_drawtools cg_effects cg_ents \
   cg_event cg_info cg_localents cg_marks cg_particles cg_players \
   cg_playerstate cg_predict cg_scoreboard cg_servercmds cg_snapshot \
-  cg_view cg_weapons ui_shared \
+  cg_view vr_cgame cg_weapons ui_shared vr_uishared \
+  vr_bg vr_platform \
   q_math q_shared
 
 UI_SRC = ui_main $(UIDIR)/ui_syscalls.asm \
-  ui_atoms ui_gameinfo ui_players ui_shared \
-  bg_misc bg_lib \
+  ui_atoms ui_gameinfo ui_players ui_shared vr_ui vr_uishared \
+  bg_misc bg_lib vr_bg vr_platform \
   q_math q_shared
 
 else
@@ -32,7 +33,8 @@ CG_SRC = cg_main $(CGDIR)/cg_syscalls.asm \
   bg_misc bg_pmove bg_slidemove bg_lib \
   cg_consolecmds cg_draw cg_drawtools cg_effects cg_ents cg_event \
   cg_info cg_localents cg_marks cg_particles cg_players cg_playerstate \
-  cg_predict cg_scoreboard cg_servercmds cg_snapshot cg_view cg_weapons \
+  cg_predict cg_scoreboard cg_servercmds cg_snapshot cg_view vr_cgame \
+  cg_weapons vr_bg vr_platform \
   q_math q_shared
 
 # Stock has no q3_ui syscalls file; the baseq3 UI links code/ui's.
@@ -44,7 +46,8 @@ UI_SRC = ui_main $(TAUIDIR)/ui_syscalls.asm \
   ui_removebots ui_saveconfig ui_serverinfo ui_servers2 ui_setup \
   ui_sound ui_sparena ui_specifyserver ui_splevel ui_sppostgame \
   ui_spskill ui_startserver ui_team ui_teamorders ui_video \
-  bg_misc bg_lib \
+  ui_vrcomfort ui_vrcontrols ui_vrhud_display ui_vrmirror ui_vroptions vr_ui \
+  bg_misc bg_lib vr_bg vr_platform \
   q_math q_shared
 
 endif
